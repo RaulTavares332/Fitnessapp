fe7fc9a43f3fe082964dba4a23ca2879808add5f
Raul Tavares <08065876318r@gmail.com>
2025-03-01 07:35:28 +0900

    Create and link Repository

---
+++ Animeted/AnimetedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimetedApp.swift
+//  Animeted
+//
+//  Created by Raul Tavares on 2025/03/01.
+//
+
+import SwiftUI
+
+@main
+struct AnimetedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimetedTests/AnimetedTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnimetedTests.swift
+//  AnimetedTests
+//
+//  Created by Raul Tavares on 2025/03/01.
+//
+
+import Testing
+@testable import Animeted
+
+struct AnimetedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
